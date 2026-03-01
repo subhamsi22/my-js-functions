4809b20d3b4526ce9a8f751a01e198ba250111f3
subhamsi <sanjusinghkk3498@gmail.com>
2026-03-01 19:27:23 +0530

    this file show the nulish

---
+++ nulish_coalescing.js
@@ -0,0 +1,5 @@
+let user;// if user did't assing it show undefine or error message 
+console.log(user ?? "not assing");
+// if user assing 
+let user_2 = "subham";
+console.log(user_2 ?? "not assing");
---
+++ type_conversion.js
@@ -0,0 +1,12 @@
+let  a  = true; // store bool value 
+console.log(typeof(a)); // show boolean value 
+a = String(a);
+console.log(typeof(a));
+// number type conversition 
+let  b  = "1";// its a String
+console.log(typeof(b));
+
+
+// convert sting to  number
+b  = Number(b); 
+console.log(typeof(b));
