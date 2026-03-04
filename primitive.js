cbf67ac66533682cc936ac1e640b9b1eacd8d0ff
subhamsi <sanjusinghkk3498@gmail.com>
2026-03-04 19:15:48 +0530

    this is a primitive show

---
+++ primitive.js
@@ -0,0 +1,4 @@
+let  c  = "hello world";// ye ek primitve data type hai
+let ss = c.toUpperCas();
+console.log(ss);// ye output me "HELLO WORLD" print karega jo 
+// ek string method hai aur ye string ko uppercase me convert kar dega
